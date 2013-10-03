7e3e6c23332948d171bcd26a193b5408166bddb2
Daniel Mendler <mail@daniel-mendler.de>
2013-10-03 17:23:48 +0200

    set metadata.language to ruby as IJulia does

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,2 +1,10 @@
-$.getScript('/static/components/codemirror/mode/ruby/ruby.js');
-IPython.CodeCell.options_default['cm_config']['mode'] = 'ruby';
+$([IPython.events]).on('notebook_loaded.Notebook', function(){
+    // add here logic that should be run once per **notebook load**
+    IPython.notebook.metadata.language = 'ruby' ;
+});
+
+$([IPython.events]).on('app_initialized.NotebookApp', function(){
+    // add here logic that shoudl be run once per **page load**
+    $.getScript('/static/components/codemirror/mode/ruby/ruby.js');
+    IPython.CodeCell.options_default['cm_config']['mode'] = 'ruby';
+});
