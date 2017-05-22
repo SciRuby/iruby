8a2f75a9582432ae5a1ff91c0a69b7e7000c80f1
Kenta Murata <mrkn@mrkn.jp>
2017-05-22 11:41:16 +0900

    Use pty to give tty for jupyter console

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,40 +1,45 @@
 require 'test_helper'
-require 'open3'
+require 'pty'
 require 'expect'
 
 class IntegrationTest < IRubyTest
   def setup
-    @stdin, @stdout, @stderr, @process = Open3.popen3('bin/iruby')
+    $expect_verbose = false # make true if you want to dump the output of iruby console
+
+    @in, @out, pid = PTY.spawn('bin/iruby --config=jupyter_console_config.py')
+    @waiter = Thread.start { Process.waitpid(pid) }
     expect 'In [', 30
     expect '1'
     expect ']:'
   end
 
   def teardown
-    @stdin.close
-    @stdout.close
-    @stderr.close
-    @process.kill
+    @in.close
+    @out.close
+    @waiter.join
   end
 
   def write(input)
-    @stdin.puts input
+    @out.puts input
   end
 
-  def expect(pattern, timeout = 1)
-    assert @stdout.expect(pattern, timeout), "#{pattern} expected"
+  def expect(pattern, timeout = 10)
+    assert @in.expect(pattern, timeout), "#{pattern} expected, but timeout"
   end
 
   def test_interaction
     write '"Hello, world!"'
     expect '"Hello, world!"'
 
+    sleep 1
     write 'puts "Hello!"'
     expect 'Hello!'
 
+    sleep 1
     write '12 + 12'
     expect '24'
 
+    sleep 1
     write 'ls'
     expect 'self.methods'
   end
