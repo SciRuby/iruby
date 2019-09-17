76b3dff339ab9fad913ddefe368808cbd5aa1366
zheng-yongping <zheng-yongping@hotmail.com>
2019-09-17 21:47:06 +0800

    refacotr: not depending on the implementation of method_source complete_expression?

--- lib/iruby/backend.rb
+++ lib/iruby/backend.rb
@@ -57,7 +57,7 @@ module IRuby
 
     def initialize
       require 'pry'
-      Pry.memory_size = 3 
+      Pry.memory_size = 3
       Pry.pager = false # Don't use the pager
       Pry.print = proc {|output, value|} # No result printing
       Pry.exception_handler = proc {|output, exception, _| }
@@ -72,11 +72,22 @@ module IRuby
         reset
         raise SystemExit
       end
+
+      # not complete exception syntex error
       unless @pry.eval_string.empty?
         syntax_error = @pry.eval_string
         @pry.reset_eval_string
         @pry.evaluate_ruby syntax_error
       end
+
+      # raise SyntaxError when check if it is complete exception
+      # evaluate again for current line number
+      if @pry.last_result_is_exception? &&
+          @pry.last_exception.is_a?(SyntaxError) &&
+          @pry.last_exception.is_a?(Pry::UserError)
+         @pry.evaluate_ruby code
+      end
+
       raise @pry.last_exception if @pry.last_result_is_exception?
       @pry.push_initial_binding unless @pry.current_binding # ensure that we have a binding
       @pry.last_result
