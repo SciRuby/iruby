821a3efe24c36a8cb3ec2b434413fcbd6d0f35f8
Robin Neumann <robin.neumann@posteo.de>
2019-10-27 10:32:30 +0100

    Drop test references for rbczmq

--- test/iruby/session_test.rb
+++ test/iruby/session_test.rb
@@ -37,12 +37,10 @@ module IRubyTest
 
     def test_without_any_session_adapter
       IRuby::SessionAdapter::CztopAdapter.stub :available?, false do
-        IRuby::SessionAdapter::RbczmqAdapter.stub :available?, false do
-          IRuby::SessionAdapter::FfirzmqAdapter.stub :available?, false do
-            IRuby::SessionAdapter::PyzmqAdapter.stub :available?, false do
-              assert_raises IRuby::SessionAdapterNotFound do
-                IRuby::Session.new(@session_config)
-              end
+        IRuby::SessionAdapter::FfirzmqAdapter.stub :available?, false do
+          IRuby::SessionAdapter::PyzmqAdapter.stub :available?, false do
+            assert_raises IRuby::SessionAdapterNotFound do
+              IRuby::Session.new(@session_config)
             end
           end
         end
