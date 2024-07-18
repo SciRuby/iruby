SciRuby/iruby

commit d54a6b7621ce2d961775f9c262605171cb1fa39a

Added `date` to header
In message header, `date` field is required.

Ref: https://jupyter-client.readthedocs.io/en/latest/messaging.html#message-header

lib/iruby/session.rb
@@ -2,6 +2,7 @@ require 'iruby/session_adapter'
 require 'iruby/session/mixin'
 
 require 'securerandom'
+require 'time'
 
 module IRuby
   class Session
@@ -79,6 +80,7 @@ module IRuby
       header = {
         msg_type: message_type,
         msg_id:   SecureRandom.uuid,
+        date:     Time.now.utc.iso8601,
         username: 'kernel',
         session:  @session_id,
         version:  '5.0'

commit fce606039b616754df69939c7ff87d03889136b1

Fixed Ruby 2.5 compatibility
Previous commit d54a6b7 broke Ruby 2.5 compatibility by using `Time#iso8601`. Patching it to use `Time#strftime` instead.

lib/iruby/session.rb
@@ -2,7 +2,6 @@ require 'iruby/session_adapter'
 require 'iruby/session/mixin'
 
 require 'securerandom'
-require 'time'
 
 module IRuby
   class Session
@@ -80,7 +79,7 @@ module IRuby
       header = {
         msg_type: message_type,
         msg_id:   SecureRandom.uuid,
-        date:     Time.now.utc.iso8601,
+        date:     Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ'),
         username: 'kernel',
         session:  @session_id,
         version:  '5.0'