c5abe6dd859d2311d45d5c6914dc6ece29b970c9
kojix2 <2xijok@gmail.com>
2026-05-30 16:32:07 +0900

    Remove legacy IRUBY_OLD_SESSION session implementations (#384)
    - Always load the current SessionAdapter-based session implementation
    - Remove the undocumented IRUBY_OLD_SESSION fallback
    - Delete old ffi-rzmq and cztop Session implementations

--- lib/iruby.rb
+++ lib/iruby.rb
@@ -19,18 +19,4 @@ require 'iruby/formatter'
 require 'iruby/utils'
 require 'iruby/display'
 require 'iruby/comm'
-
-if ENV.fetch('IRUBY_OLD_SESSION', false)
-  require 'iruby/session/mixin'
-  begin
-    require 'iruby/session/ffi_rzmq'
-  rescue LoadError
-    begin
-      require 'iruby/session/cztop'
-    rescue LoadError
-      STDERR.puts "Please install ffi-rzmq or cztop before running iruby. See README."
-    end
-  end
-else
-  require 'iruby/session'
-end
+require 'iruby/session'
